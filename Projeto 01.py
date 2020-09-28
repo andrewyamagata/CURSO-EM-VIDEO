6832ce9455f0c78e9da30d4237f6ef2c941c4c27
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-09-28 20:06:56 -0300

    Aula Básico parte 01

--- Projeto 01.py
+++ Projeto 01.py
@@ -1,3 +1,17 @@
-print("Hello World")
-print("Welcome")
 print("Olá Mundo")
+print(7+4)
+print('7'+'4')
+
+nome = input('Qual o seu nome?')
+ano = int(input('Em que ano você nasceu?'))
+idade = 2020 - ano
+print('Olá',nome,'você está com',idade,'de idade')
+print('Está ficando velho meu amigo, {} anos já!'.format(idade))
+
+a = input('Digite algo:')
+print('O tipo primitivo desse valor é ', type(a))
+print('É alfanumérico? ',a.isalnum())
+print('É alfabético? ',a.isalpha())
+print('É numérico? ', a.isnumeric())
+print('Está em maiúsculas? ', a.isupper())
+print('Está em minúsculas? ', a.islower())

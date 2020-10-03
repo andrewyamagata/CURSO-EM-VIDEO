1d904fb4bed026bda47c2c194bac463f9fe67957
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-03 00:04:54 -0300

    Módulo

--- Projeto 03.py
+++ Projeto 03.py
@@ -23,5 +23,10 @@ print(nrandom)
 '''from random import randint
 nrandom = randint(1,60)      => não colocar random, já importou randint
 print(nrandom)'''
+
 #pypi módulos externos
 # Settings -> Project: Python -> Python Interpreter -> +
+
+angulo = float(input('Digite o ângulo que você deseja: '))
+seno = math.sin(math.radians(angulo)) #transformar em radianos o angulo e achar o seno
+print('O ângulo de {} tem o Seno de {:.2f}'.format(angulo,seno))

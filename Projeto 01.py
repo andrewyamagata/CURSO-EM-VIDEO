2d753749949ee79d18ff0f9c5eacb3fd04442126
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-02 22:50:12 -0300

    Introdução ao Python

--- Projeto 01.py
+++ Projeto 01.py
@@ -7,6 +7,11 @@ ano = int(input('Em que ano você nasceu?'))
 idade = 2020 - ano
 print('Olá',nome,'você está com',idade,'de idade')
 print('Está ficando velho meu amigo, {} anos já!'.format(idade))
+print('Prazer em te conhecer {:20}!'.format(nome))
+print('Prazer em te conhecer {:>20}!'.format(nome))
+print('Prazer em te conhecer {:<20}!'.format(nome))
+print('Prazer em te conhecer {:^20}!'.format(nome))
+print('Prazer em te conhecer {:=^20}!'.format(nome))
 
 a = input('Digite algo:')
 print('O tipo primitivo desse valor é ', type(a))
@@ -15,6 +20,7 @@ print('É alfabético? ',a.isalpha())
 print('É numérico? ', a.isnumeric())
 print('Está em maiúsculas? ', a.isupper())
 print('Está em minúsculas? ', a.islower())
+
 # + adição
 # - subtração
 # * mutiplicação
@@ -22,3 +28,14 @@ print('Está em minúsculas? ', a.islower())
 # ** potência
 # // divisão inteira
 # % resto da divisão
+
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma vale {}'.format(n1+n2))
+print('A soma vale {}, o produto vale {}, e a divisão vale {:.3}'.format(s,m,d)) #3 casas decimais
+print('Divisão inteira {} e potência {}'.format(di,e))

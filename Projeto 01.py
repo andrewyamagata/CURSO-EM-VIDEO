6cd58323cc3d4b7e44cfdcb95718c7d40fabd0ad
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-02 23:13:34 -0300

    introdução Python

--- Projeto 01.py
+++ Projeto 01.py
@@ -20,26 +20,3 @@ print('É alfabético? ',a.isalpha())
 print('É numérico? ', a.isnumeric())
 print('Está em maiúsculas? ', a.isupper())
 print('Está em minúsculas? ', a.islower())
-
-# + adição
-# - subtração
-# * mutiplicação
-# / divisão
-# ** potência
-# // divisão inteira
-# % resto da divisão
-
-n1 = int(input('Primeiro valor: '))
-n2 = int(input('Segundo valor: '))
-s = n1 + n2
-m = n1 * n2
-d = n1 / n2
-di = n1 // n2
-e = n1 ** n2
-print('A soma vale {}'.format(n1+n2))
-print('A soma vale {}, o produto vale {}, e a divisão vale {:.3f}'.format(s,m,d)) #3 casas decimais
-print('Divisão inteira {} e potência {}'.format(di,e))
-print('A soma vale {}, o produto vale {}, e a divisão vale {:.3f}'.format(s,m,d), end=' ')#end=' ' não quebrar linha, colocar dentro da aspas
-print('Divisão inteira {} e potência {}'.format(di,e))
-print('A soma vale {}, \n o produto vale {}, \n e a divisão vale {:.3f}'.format(s,m,d)) #\n quebra linha
-print('Divisão inteira {} e potência {}'.format(di,e))

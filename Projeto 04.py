14954224546eee53d427661557f812f3f6b55922
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-03 00:16:49 -0300

    Módulo p2

--- Projeto 04.py
+++ Projeto 04.py
@@ -7,3 +7,10 @@ n4 = str(input('Quarto aluno: '))
 lista = [n1,n2,n3,n4] #criar uma lista, deixar em colchetes
 escolha = random.choice(lista) #escolhe uma variável aleatória
 print('O aluno escolhido foi {}!'.format(escolha))
+
+#Sorteio e colocar em ordem
+random.shuffle(lista)
+print('A ordem será:')
+print(lista)
+
+# Colocar música mp3

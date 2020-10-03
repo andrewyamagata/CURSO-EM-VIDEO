98ce6dcc956e28e7304a91999c6b0de87c4e7579
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-02 23:55:24 -0300

    Módulo

--- Projeto 03.py
+++ Projeto 03.py
@@ -10,8 +10,16 @@
 #importar tudo => import math
 #importar funcionalidade especifica => from math import sqrt
 #https://docs.python.org/pt-br/3.7/library/index.html biblioteca para importar
+#ctrl + espaço mostra lista
 
 import math
 num = int(input('Digite um número: '))
 raiz = math.sqrt(num)
 print('A raiz de {} é {:.2f}!'.format(num, (raiz)))
+
+import random
+nrandom = random.randint(1,60)
+print(nrandom)
+
+#pypi módulos externos
+# Settings -> Project: Python -> Python Interpreter -> +

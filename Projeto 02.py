8fd29ed14326beff0840535211a221e5a79d25d2
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-02 23:11:01 -0300

    Introdução ao Python p2

--- Projeto 02.py
+++ Projeto 02.py
@@ -1,2 +1,3 @@
 n = int(input('Digite um valor: '))
-print('Número anterior {} e número posterior {} ao número escolhido {}'.format(n-1,n+1,n))
+print('antecessor {} e sucessor {}, ao número escolhido {}!'.format((n-1),(n+1),n))
+print('Dobro {}\nTriplo {}\nRaiz quadrada {:.2f}!'.format((n*2),(n*3),(n**(1/2))))#pow(n, (1/2) também calcula raiz quadrada

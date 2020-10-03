c783c6db49bc78b66fc57e992082b47cca7d610d
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-03 00:11:54 -0300

    Módulo

---
+++ Projeto 04.py
@@ -0,0 +1,9 @@
+#Sorteando
+import random
+n1 = str(input('Primeiro aluno: ')) #string, não necessário colocar
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+lista = [n1,n2,n3,n4] #criar uma lista, deixar em colchetes
+escolha = random.choice(lista) #escolhe uma variável aleatória
+print('O aluno escolhido foi {}!'.format(escolha))

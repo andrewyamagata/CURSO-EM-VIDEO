ab00b1036e45457d89072c97c6955fec90064892
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-02 23:39:06 -0300

    Módulo

---
+++ Projeto 03.py
@@ -0,0 +1,12 @@
+# Instalando módulos
+
+#ceil arredonda p cima
+#floor arredonda p baixo
+#trunc truncar
+#pow potencia
+#sqrt raiz quadrada
+#factorial fatorial
+
+#importar tudo => import math
+#importar funcionalidade especifica => from math import sqrt
+

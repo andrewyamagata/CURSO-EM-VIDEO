a4d9b1094679ae8e95d9935fcb5782c8c3c65739
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-02 23:45:37 -0300

    Módulo

--- Projeto 03.py
+++ Projeto 03.py
@@ -9,4 +9,9 @@
 
 #importar tudo => import math
 #importar funcionalidade especifica => from math import sqrt
+#https://docs.python.org/pt-br/3.7/library/index.html biblioteca para importar
 
+import math
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('A raiz de {} é {:.2f}!'.format(num, (raiz)))

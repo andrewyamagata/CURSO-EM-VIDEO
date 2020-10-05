767db5011cc47510bac42b91dcaa9d8218faf3e3
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-04 23:10:39 -0300

    Manipulando texto

--- Projeto 06.py
+++ Projeto 06.py
@@ -1,5 +1,11 @@
 #Manipulando texto
 #[] indica lista => começa em 0
 #=>frase Curso em Video Python
+frase = input('Digite:')
+print(frase[9:13])
 #[9:13] vai do 9 até o 12, o 13 não pega (ultimo valor nao pega), no caso = Vide
+print(frase[9:21])
 #[9:21] nao tem 21, vai até o ultimo, neste caso = Video Python
+print(frase[9:21:2])
+#[9:21:2] começa no 9 e para no 21, pulando de 2 em 2 = VdoPto
+

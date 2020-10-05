dd813593289e1f615ef2402811c1b00430f04b8f
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-04 23:06:16 -0300

    Manipulando texto

--- Projeto 06.py
+++ Projeto 06.py
@@ -1,5 +1,5 @@
 #Manipulando texto
 #[] indica lista => começa em 0
 #=>frase Curso em Video Python
-#[9:13] vai do 9 até o 12, o 13 não pega(ultimo valor nao pega) Vide
-#[9:21]
+#[9:13] vai do 9 até o 12, o 13 não pega (ultimo valor nao pega), no caso = Vide
+#[9:21] nao tem 21, vai até o ultimo, neste caso = Video Python

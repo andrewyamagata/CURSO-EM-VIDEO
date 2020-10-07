85b4c01639fa951a960f28cf57049a7579b4c4c7
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-06 22:17:10 -0300

    Verificando a cadeia de caracteres - string

---
+++ Projeto 10.py
@@ -0,0 +1,12 @@
+#Condições (if - else)
+tempo = int(input('Quantos anos tem o seu carro? '))
+if tempo <= 3:
+    print('Seu carro está novinho, pense em trocar daqui {} anos'.format(3 - tempo ))
+else:
+    print('Já está na hora de trocar o seu carro, ele está obsóleto faz {} anos'.format(tempo - 3))
+print('----FIM----')
+
+#pode ser feito de outra forma
+tempo = int(input('Quantos anos tem seu carro? '))
+print('carro novo' if tempo <= 3 else 'carro velho')
+print('--END--')

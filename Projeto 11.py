49a1718001261e4879c6dfd167b429f8eb2b9286
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-06 23:09:03 -0300

    Condições exercícios

--- Projeto 11.py
+++ Projeto 11.py
@@ -6,3 +6,11 @@ else:
     print('Multado! Você excedeu o limite de velocidade de 80 km/h')
     print('Você deverá pagar uma multa de R$ {:.2f}'.format((velocidade - 80)*7))#número decimal com duas casas flutuante
 print('Tenha um bom dia!')
+
+#Sabendo se é par ou ímpar
+numero = int(input('Me diga um número: '))
+resultado = numero % 2 #Resto da divisão 0 ou 1.
+if  resultado == 0:
+    print('O número {} é PAR'.format(numero))
+else:
+    print('O número {} é ÍMPAR'.format(numero))

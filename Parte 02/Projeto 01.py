6db04aac3354df4ee865eb505226a9ecdb06e318
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-07 23:29:40 -0300

    Condições de aninhamento

--- Parte 02/Projeto 01.py
+++ Parte 02/Projeto 01.py
@@ -6,4 +6,7 @@ casa = float(input('Qual o valor da casa: R$ '))
 salario = float(input('Qual o valor do salário: R$ '))
 tempo = int(input('Quanto anos para pagar? '))
 salario30 = salario * 0.30
-if
+prestacao = casa / (tempo * 12)
+#if
+
+print('Para pagar a casa de R$ {:.2f}, com o salário que recebe de R$ {:.2f}, em {} anos, você deve pagar mensalmente R$ {}'.format(casa,salario,tempo,prestacao))

e87dd4f1168754e41f94b271e526531e57312d2d
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-07 23:24:02 -0300

    Parte 02

--- Parte 02/Projeto 01.py
+++ Parte 02/Projeto 01.py
@@ -1,2 +1,9 @@
 #Condições Aninhadas
 #if -> elif -> elif -> else # pode usar quantos elif quiser
+
+#Aprovando Empréstimo
+casa = float(input('Qual o valor da casa: R$ '))
+salario = float(input('Qual o valor do salário: R$ '))
+tempo = int(input('Quanto anos para pagar? '))
+salario30 = salario * 0.30
+if

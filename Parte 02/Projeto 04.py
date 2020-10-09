cf862a16ad1dde8070108f99397e3655bbacc5d8
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-09 00:07:00 -0300

    Alistamento militar

--- Parte 02/Projeto 04.py
+++ Parte 02/Projeto 04.py
@@ -3,3 +3,9 @@ import datetime
 ano = int(input('Ano de nascimento: '))
 idade = datetime.date.today().year - ano
 print('Quem nasceu em {} tem {} anos'.format(ano, idade))
+if idade > 18:
+    print('Você deveria ter se apresentado no alistamento há {} anos atrás'.format(idade - 18))
+elif idade < 18:
+    print('Você ainda irá se alistar daqui {} anos'.format(18 - idade))
+else:
+    print('Você já tem {} anos, este ano você deve se alistar'.format(idade))

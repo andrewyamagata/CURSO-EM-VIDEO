ae3eb4ff3fa68560c1ebd1781ee264e04840cdbf
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-09 18:08:05 -0300

    Classificação atletas

--- Parte 02/Projeto 06.py
+++ Parte 02/Projeto 06.py
@@ -1,1 +1,16 @@
 #Classíficando
+from datetime import date
+atual = date.today().year
+ano = int(input('Em que ano você nasceu? '))
+idade = atual - ano
+if idade <= 9: #< depois =
+    categoria = ("MIRIM")
+elif idade <= 14:
+    categoria = ("INFANTIL")
+elif idade <=19:
+    categoria = ("JUNIOR")
+elif idade <= 25:
+    categoria = ("SÊNIOR")
+else:
+    categoria = ("MASTER")
+print("Você está com {} anos e a sua categoria é {}!".format(idade,categoria))

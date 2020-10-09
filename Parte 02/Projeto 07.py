12023a5c20dd3689021cc3939b5d902b5a7c8b23
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-09 18:25:05 -0300

    Analisando Triângulos - Condição Aninhada

--- Parte 02/Projeto 07.py
+++ Parte 02/Projeto 07.py
@@ -3,7 +3,7 @@ l1 = float(input('Digite a medida do primeiro lado: '))
 l2 = float(input('Digite a medida do segundo lado: '))
 l3 = float(input('Digite a medida do terceiro lado: '))
 if l1 < l2 + l3 and l2 < l1 + l3 and l3 < l1 + l2:
-    print('Os segmentos formam um Triângulo')
+    print('Os segmentos formam um Triângulo', end=' ')
     if l1 == l2 and l2 == l3 and l1 == l3:
         print("EQUILÁTERO")
     elif l1 != l2 and l2 != l3 and l1 != l3:

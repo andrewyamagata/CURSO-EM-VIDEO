14de67d7d2ee736f9891248e77063fd88836a316
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-10 17:25:41 -0300

    Contagem regressiva

--- Parte 02/Projeto 11.py
+++ Parte 02/Projeto 11.py
@@ -3,5 +3,10 @@ import time
 print('Contagem regressiva')
 for c in range(10,-1, -1):
     print(c)
-    time.sleep(1)
+    time.sleep(0.5)
 print("BUUUUUM")
+
+#Contagem pares
+for d in range(1,51): # ou range(2, 51, 2) de 2 até 50 pulando de 2 em 2, neste caso apresenta números pares
+    if  d % 2 == 0:
+        print(d, end=" ")

15deb4ace8ae335427f585517852b51831f1df99
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-10 15:57:33 -0300

    Estrutura de repetição

--- Parte 02/Projeto 10.py
+++ Parte 02/Projeto 10.py
@@ -1,2 +1,21 @@
 #Estrutura de repetição for
 #estrutura de laço repetição
+for c in range(1, 6): #laço c=>"variavel de controle" no intervalo(1, 6), sem o 6
+    #range(6,0,-1)começar do 6 até 0,
+    print('OI')
+    print(c)
+print('FIM')
+print(c)
+
+#vai do 0 até o número escolhido +1
+n = int(input('Digite um número: '))
+for d in range(0, n+1):
+    print(d)
+print('Fim')
+
+#Somatorio
+s = 0
+for e in range(0,4):
+    n = int(input("Digite um valor: "))
+    s = s + n #ou s += n
+print('O somatório foi de {}'.format(s))

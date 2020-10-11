2d6a54fe8d417c9e9baa8060b444b705604bd5cf
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-11 18:30:05 -0300

    Progressão Aritmética

--- Parte 02/Projeto 13.py
+++ Parte 02/Projeto 13.py
@@ -7,3 +7,19 @@ razao = int(input('Razão: '))
 decimo = termo + (10-1) * razao #achar o decimo termo
 for c in range(termo,decimo + razao ,razao):
     print("{} ".format(c),end="- ")
+print("\nAcabou")
+
+#Números primos
+numero = int(input("Digite um número: "))
+cont = 0
+for d in range(1,numero+1):
+    if numero % d == 0:
+        print("\033[33m", end=" ")
+        cont = cont +1
+    else:
+        print("\033[31m", end=" ")
+    print("{} ".format(d), end=" ")
+if cont == 2:
+    print("\n\033[34mO número {} é PRIMO".format(numero))
+else:
+    print("\n\033[36mO númeo {} não é PRIMO, sendo divisível por {} números".format(numero,cont))

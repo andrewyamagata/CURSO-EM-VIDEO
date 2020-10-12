d4d7f8259ad27ecb84cd0f891b02521ac939f009
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-12 16:23:55 -0300

    criação de Jogo

---
+++ Projeto 01/jogo_parte1.py
@@ -0,0 +1,14 @@
+#Criando em jogo em python
+import pygame
+pygame.init()
+
+janela = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Criando um jogo com Python")
+
+janela_aberta = True
+while janela_aberta:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            janela_aberta = False
+
+pygame.quit()

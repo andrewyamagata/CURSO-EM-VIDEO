8e3db342716e3671b3861047d61142704d98f779
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-13 19:24:16 -0300

    Progressão Aritmética

--- Parte 02/Projeto 22.py
+++ Parte 02/Projeto 22.py
@@ -1,1 +1,12 @@
 #Progressão Aritmética v2
+print("Gerador de PA")
+print("-="*12)
+primeiro = int(input("Primeiro termo: "))
+razão = int(input("Razão da PA: "))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print("{} ->".format(termo),end=" ")
+    termo += razão
+    cont += 1
+print("Fim")

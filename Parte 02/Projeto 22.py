6d9c760d60c521f87ef0b02eddd38ac793c5315b
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-13 22:49:49 -0300

    Progressão Aritmética

--- Parte 02/Projeto 22.py
+++ Parte 02/Projeto 22.py
@@ -1,5 +1,5 @@
 #Progressão Aritmética v2
-print("Gerador de PA")
+"""print("Gerador de PA")
 print("-="*12)
 primeiro = int(input("Primeiro termo: "))
 razão = int(input("Razão da PA: "))
@@ -9,4 +9,23 @@ while cont <= 10:
     print("{} ->".format(termo),end=" ")
     termo += razão
     cont += 1
-print("Fim")
+print("Fim")"""
+
+#Progressão Aritmética v3
+print("Gerador de PA")
+print("-="*12)
+primeiro = int(input("Primeiro termo: "))
+razão = int(input("Razão da PA: "))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print("{} ->".format(termo),end=" ")
+        termo += razão
+        cont += 1
+    print("Pausa")
+    mais = int(input("Quantos termos você quer mostrar a mais? "))
+print("PA finalizada com {} termos mostrados".format(total))

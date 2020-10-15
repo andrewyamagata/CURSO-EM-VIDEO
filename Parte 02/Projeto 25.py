1e76c42e81635ffcc95045db4f5572fbe8c330e7
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-14 22:17:43 -0300

    Maior e Menor Valores

--- Parte 02/Projeto 25.py
+++ Parte 02/Projeto 25.py
@@ -1,1 +1,7 @@
 #maior e menor valores
+cont = 0
+while opcao == "S":
+    n = int(input("Digite um número: "))
+    opcao = str(input("Quer continuar [S/N]? ")).strip().upper()[0]#[]0 considera 1° letra
+    cont += 1
+print("Acabou")

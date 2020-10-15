ca8734d6e6b3166422374787cb7d33e2e7360777
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-14 22:02:46 -0300

    Tratando vários valores

--- Parte 02/Projeto 24.py
+++ Parte 02/Projeto 24.py
@@ -1,7 +1,16 @@
 #Tratando vários valores
-num = cont = soma = 0
+"""num = cont = soma = 0
 while num != 999:
     num = int(input("Digite um número [999 para parar]: "))
     soma += num
     cont += 1
-print("Você digitou {} números e a soma deu {}".format(cont-1,soma-999))
+print("Você digitou {} números e a soma deu {}".format(cont-1,soma-999))"""
+
+#pode ser feito dessa maneira
+num = cont = soma = 0
+num = int(input("Digite um número [999 para parar]: ")) #coloca a flag p fora
+while num != 999:
+    soma += num
+    cont += 1
+    num = int(input("Digite um número [999 para parar]: ")) #ler como última linha
+print("Você digitou {} números e a soma deu {}".format(cont,soma))

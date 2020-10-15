c5cfda4bb237fef23b30a635d0e4ce58010f7ca8
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-14 21:59:56 -0300

    Tratando vários valores

--- Parte 02/Projeto 24.py
+++ Parte 02/Projeto 24.py
@@ -1,1 +1,7 @@
 #Tratando vários valores
+num = cont = soma = 0
+while num != 999:
+    num = int(input("Digite um número [999 para parar]: "))
+    soma += num
+    cont += 1
+print("Você digitou {} números e a soma deu {}".format(cont-1,soma-999))

7a5e816a104d8763d07a1036d99111e90aa66f84
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-20 22:39:20 -0200

    Interrompendo repetições while

--- Parte 02/Projeto 26.py
+++ Parte 02/Projeto 26.py
@@ -5,3 +5,9 @@ while cont != 10:
     cont += 1
 print('Acabou')
 #nao ser infinito
+
+nome = 'Jose'
+idade = 33
+print(f'O {nome} tem {idade} anos. Forma 01') #Python 3.6+
+print('O {} tem {} anos. Forma 2', format(nome, idade)) #Python 3
+print('O %s tem %d anos' % (nome, idade)) #Python 2

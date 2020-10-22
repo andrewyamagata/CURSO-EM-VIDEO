bb640acf2c147be28a89daf683282ad869852f22
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-22 18:56:05 -0200

    Tabuada v3

--- Parte 02/Projeto 28.py
+++ Parte 02/Projeto 28.py
@@ -1,10 +1,10 @@
 #Tabuada v3
 while True:
     n = int(input('Digite qual tabuada você quer: '))
+    print('-'*30)
     if n < 0:
         print("PROGRAMA ENCERRADO")
         break
-    print('-'*30)
     for c in range(1,11):
         print(f'{n} x {c} = {n*c}')
     print('-' * 30)

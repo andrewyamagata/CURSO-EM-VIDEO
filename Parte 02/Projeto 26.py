070b721e5f7f13ca0171444304f5a6c16fed364e
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-21 23:22:01 -0200

    Interrompendo repetições while

--- Parte 02/Projeto 26.py
+++ Parte 02/Projeto 26.py
@@ -17,7 +17,8 @@ print('A soma vale {}'.format(s))
 #f string
 nome = 'Jose'
 idade = 33
-
+salario = 873.5
 print(f'O {nome} tem {idade} anos. Forma 01') #Python 3.6+
 print('O {} tem {} anos. Forma 02'.format(nome, idade)) #Python 3
 print('O %s tem %d anos. Forma 03' % (nome, idade)) #Python 2
+print(f'O sálario do {nome:^20}é de R$ {salario:.2f} ')#colocar o :.2f

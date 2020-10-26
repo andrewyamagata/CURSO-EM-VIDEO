68c722875ee430b71bc4abe02ac424a4c770952d
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-25 23:33:19 -0200

    Análise de dados

--- Parte 02/Projeto 30.py
+++ Parte 02/Projeto 30.py
@@ -1,1 +1,6 @@
 #Análise de dados do grupo
+while True:
+    idade = int(input('Idade: '))
+    sexo = ' '
+    while sexo not in 'MF'  #enquanto não digitar M ou F
+        sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]

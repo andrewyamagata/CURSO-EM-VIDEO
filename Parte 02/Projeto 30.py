5e1dda53a7be1b2e55fac4017eb4af8cd456a548
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-25 23:34:17 -0200

    Análise de dados

--- Parte 02/Projeto 30.py
+++ Parte 02/Projeto 30.py
@@ -2,5 +2,5 @@
 while True:
     idade = int(input('Idade: '))
     sexo = ' '
-    while sexo not in 'MF'  #enquanto não digitar M ou F
+    while sexo not in 'MF':  #enquanto não digitar M ou F
         sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]

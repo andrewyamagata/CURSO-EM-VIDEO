36b3869f41a47e443f7c5b46686e39b3b71cd608
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-26 23:18:01 -0300

    Estatística em produtos

--- Parte 02/Projeto 31.py
+++ Parte 02/Projeto 31.py
@@ -3,3 +3,6 @@ loja = 'LOJA SUPER BARATÃO'
 print('-'*50)
 print('{:^50}'.format(loja))
 print('-'*50)
+while True:
+    produto = str(input('Nome do produto: '))
+    preco = float(input('Preço: R$ '))

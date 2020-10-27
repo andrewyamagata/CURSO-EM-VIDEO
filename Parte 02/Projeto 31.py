440ec972ce6472d6d2d1b4952667acdda38daead
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-26 23:25:26 -0300

    Estatística em produtos

--- Parte 02/Projeto 31.py
+++ Parte 02/Projeto 31.py
@@ -2,13 +2,16 @@
 print('-'*50)
 print('{:^50}'.format('LOJA SUPER BARATÃO'))
 print('-'*50)
+total = 0
 while True:
     produto = str(input('Nome do produto: '))
     preco = float(input('Preço: R$ '))
+    total += preco
 
     resp = ' ' #resp recebe vazio
     while resp not in 'SN':
         resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     if resp == 'N':
         break
-print()
+print('O total gasto foi de {:2f}'.format(total))
+print('{:-^50}'.format('FIM DO PROGRAMA'))

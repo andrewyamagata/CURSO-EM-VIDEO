f1f7f75bfb43c720258312280df5b9d22727d64d
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-26 22:23:27 -0200

    Análise de dados

--- Parte 02/Projeto 30.py
+++ Parte 02/Projeto 30.py
@@ -6,14 +6,17 @@ while True:
     print('-'*30)
     idade = int(input('Idade: '))
     sexo = ' '
-    if idade == 0:
-        break
     while sexo not in 'MF':  #enquanto não digitar M ou F
         sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
-        if idade > 18:
-            cont18 += 1
-        if sexo == 'M':
-            contm += 1
-        if sexo == 'F' and idade < 20:
-            contf20 += 1
+    if idade > 18:
+        cont18 += 1
+    if sexo == 'M':
+        contm += 1
+    if sexo == 'F' and idade < 20:
+        contf20 += 1
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input("Quer continuar? [S/N} ")).strip().upper()[0]
+    if resp == 'N':
+        break
 print(f'Do total {cont18} tem mais de 18 anos, {contm} são homens, {contf20} são mulheres com menos de 20 anos.')

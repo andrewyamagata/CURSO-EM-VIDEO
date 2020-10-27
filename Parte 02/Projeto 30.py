33a0c57ad3b41c7ba9b98cc01be9aaedff2a143b
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-26 22:02:05 -0200

    Análise de dados

--- Parte 02/Projeto 30.py
+++ Parte 02/Projeto 30.py
@@ -1,5 +1,8 @@
 #Análise de dados do grupo
 while True:
+    print('-'*30)
+    print('CADASTRE UMA PESSOA')
+    print('-'*30)
     idade = int(input('Idade: '))
     sexo = ' '
     while sexo not in 'MF':  #enquanto não digitar M ou F

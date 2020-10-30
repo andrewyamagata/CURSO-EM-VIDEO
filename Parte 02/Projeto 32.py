0718ef7074ec029279a6a3ac43ccfc914cc0b48a
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-10-30 00:00:34 -0300

    Simulado de Caixa Eletrônico

--- Parte 02/Projeto 32.py
+++ Parte 02/Projeto 32.py
@@ -6,3 +6,9 @@ valor = int(input('Que valor você quer sacar? R$ '))
 total = valor
 ced = 50
 totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if

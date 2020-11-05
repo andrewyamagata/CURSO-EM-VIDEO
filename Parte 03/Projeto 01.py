5c1ba622a0d313b9d6a842d04409e72afb4e6d8e
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-11-04 23:10:40 -0300

    Tuplas

--- Parte 03/Projeto 01.py
+++ Parte 03/Projeto 01.py
@@ -1,5 +1,5 @@
 #Tuplas
 lanche = ('hambúrger', 'suco', 'pizza', 'pudim')
-print(lanche)
-print(lanche[3])
-print(lanche[:2])
+print(lanche) #todos os itens
+print(lanche[3]) #item na posição 4 (item 3)
+print(lanche[:2]) #pulando de 2 em 2, começando de 0

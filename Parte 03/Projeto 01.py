ffb52a77c9197a6437464c51624e4cd1311caaa2
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-11-04 23:14:19 -0300

    Tuplas

--- Parte 03/Projeto 01.py
+++ Parte 03/Projeto 01.py
@@ -1,5 +1,6 @@
-#Tuplas
+#Tuplas, variável composta
 lanche = ('hambúrger', 'suco', 'pizza', 'pudim')
 print(lanche) #todos os itens
 print(lanche[3]) #item na posição 4 (item 3)
-print(lanche[:2]) #pulando de 2 em 2, começando de 0
+print(lanche[:2]) #vai do 0 até o 1
+print(len(lanche))

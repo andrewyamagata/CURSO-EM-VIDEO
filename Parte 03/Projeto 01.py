5c4d9fd243f54410989a1487a10a994eb8901605
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-11-05 21:08:51 -0300

    Tuplas

--- Parte 03/Projeto 01.py
+++ Parte 03/Projeto 01.py
@@ -1,8 +1,10 @@
-#Tuplas, variável composta entre parenteses
+#Tuplas, variável composta entre parenteses, são imutáveis
 lanche = ('hambúrger', 'suco', 'pizza', 'pudim')
 print(lanche) #todos os itens
 print(lanche[3]) #item na posição 4 (item 3)
-print(lanche[:2]) #vai do 0 até o 1
+print(lanche[-1])#de trás p frente
+print(lanche[:2]) #vai do 0 até o 1, ignorando o ultimo elemente, neste caso o 2
+print(lanche[1:3])#vai do 1 até o 2 , ignorando o ultimo elemento, neste caso o 3
 print(len(lanche))
 for c in lanche:
     print(c)

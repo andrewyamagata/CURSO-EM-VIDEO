f567ce2316d332801ab8e787cc7f4eff6151853d
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-11-06 22:51:15 -0300

    Tuplas

--- Parte 03/Projeto 01.py
+++ Parte 03/Projeto 01.py
@@ -6,11 +6,13 @@ print(lanche[-1])#de trás p frente
 print(lanche[:2]) #vai do 0 até o 1, ignorando o ultimo elemente, neste caso o 2
 print(lanche[1:3])#vai do 1 até o 2 , ignorando o ultimo elemento, neste caso o 3
 print(len(lanche))
+print('-'*30)
 for c in lanche:
     print(f'Como {c}')
-
+print('-'*30)
 for cont in range(0, len(lanche)):
     print(f'Eu comi {lanche[cont]} na posição {cont}')
-
+print('-'*30)
 for pos, comida in enumerate(lanche):
     print(f'Eu vou comer {comida} na posição {pos}')
+print('-'*30)

fd3a29cc31a5c85c3893df8b86ac6a33a50db73b
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-11-22 19:59:24 -0300

    Números por extenso

--- Parte 03/Projeto 02.py
+++ Parte 03/Projeto 02.py
@@ -7,12 +7,12 @@ lista = ('zero', 'um', 'dois', 'três', 'quatro',
          'dezenove', 'vinte')
 while True:
     n = int(input('Digite um número entre 0 e 20:  '))
-    if 0 <= n <= 20:
-        break
-    print('Tente novamente. ', end='')
-    print(f'Você digitou o número {lista[n]}')
+    if n < 0 or n > 20:
+        print('Tente novamente. ', end='')
+    else:
+        print(f'Você digitou o número {lista[n]}')
     resp = ' '
     while resp not in 'SN':
         resp = str(input('Desejá continuar? [S/N] ')).strip().upper()[0]
-        if resp == 'N':
-            break
+    if resp == 'N':
+        break

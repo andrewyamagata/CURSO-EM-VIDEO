e44298d6af1fcffaca25eb761e26aeae22658ea6
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-12-03 21:34:25 -0300

    Números por extenso

--- Parte 03/Projeto 02.py
+++ Parte 03/Projeto 02.py
@@ -1,5 +1,5 @@
 #Número por extenso
-
+'''
 lista = ('zero', 'um', 'dois', 'três', 'quatro',
          'cinco' , 'seis', 'sete' , 'oito', 'nove',
          'dez', 'onze', 'doze', 'treze', 'catorze',
@@ -15,4 +15,31 @@ while True:
     while resp not in 'SN':
         resp = str(input('Desejá continuar? [S/N] ')).strip().upper()[0]
     if resp == 'N':
-        break
+        break'''
+'''lista = ['abc', ['d'], ['ef']]
+print(lista[2][0][0])'''
+
+'''class Fracao:
+    def __init__(self, num, den):
+        self.num = num
+        self.den = den
+        
+    def __repr__(self):
+        return str(self.num) + '/' + str(self.den)
+
+f1 = Fracao(1, 2)
+f2 = Fracao(3, 4)
+print(f1)
+print(f2)'''
+
+class HORARIO:
+    def __init__(self, hora, minu, segundo):
+        self.hora = hora
+        self.minu = minu
+        self.segundo = segundo
+
+    def __repr__(self):
+        return str(self.hora) + ':' + str(self.minu) + ':' + str(self.segundo)
+
+h = HORARIO(6, 28, 45)
+print(h)

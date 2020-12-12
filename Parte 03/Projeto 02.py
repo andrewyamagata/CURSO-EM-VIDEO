dc3abba61d1194b0d05a96945dd222b4fb4ed3ea
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-12-12 17:32:11 -0300

    Prova Univesp

--- Parte 03/Projeto 02.py
+++ Parte 03/Projeto 02.py
@@ -16,30 +16,3 @@ while True:
         resp = str(input('Desejá continuar? [S/N] ')).strip().upper()[0]
     if resp == 'N':
         break
-lista = ['abc', ['d'], ['ef']]
-print(lista[2][0][0])
-
-class Fracao:
-    def __init__(self, num, den):
-        self.num = num
-        self.den = den
-        
-    def __repr__(self):
-        return str(self.num) + '/' + str(self.den)
-
-f1 = Fracao(1, 2)
-f2 = Fracao(3, 4)
-print(f1)
-print(f2)
-
-class HORARIO:
-    def __init__(self, hora, minu, segundo):
-        self.hora = hora
-        self.minu = minu
-        self.segundo = segundo
-
-    def __repr__(self):
-        return str(self.hora) + ':' + str(self.minu) + ':' + str(self.segundo)
-
-h = HORARIO(6, 28, 45)
-print(h)

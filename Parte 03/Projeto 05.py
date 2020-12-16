81237903a3699786784260719b1409b0ade44be7
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-12-15 21:20:15 -0300

    Análise de dados em uma Tupla

--- Parte 03/Projeto 05.py
+++ Parte 03/Projeto 05.py
@@ -1,1 +1,5 @@
 #Análise de dados em uma tupla
+num = (int(input("Digite um número: ")),
+       int(input("Digite um número: ")),
+       int(input("Digite um número: ")),
+       int(input("Digite um número: ")))

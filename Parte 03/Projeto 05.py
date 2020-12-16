93e5ffa8313789b6ba4e82981308b0c4d73b3570
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-12-15 21:22:16 -0300

    Análise de dados em uma Tupla

--- Parte 03/Projeto 05.py
+++ Parte 03/Projeto 05.py
@@ -4,3 +4,4 @@ num = (int(input("Digite um número: ")),
        int(input("Digite um número: ")),
        int(input("Digite um número: ")))
 print(f'Você digitou os valores {num}')
+print(f'O valor 9 apareceu {num.count(9)} vezes')

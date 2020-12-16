6c4dc86f23892f8e779bc984406b01cbe8854b4a
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2020-12-15 21:26:29 -0300

    Análise de dados em uma Tupla

--- Parte 03/Projeto 05.py
+++ Parte 03/Projeto 05.py
@@ -5,3 +5,11 @@ num = (int(input("Digite um número: ")),
        int(input("Digite um número: ")))
 print(f'Você digitou os valores {num}')
 print(f'O valor 9 apareceu {num.count(9)} vezes')
+if 3 in num:
+       print(f'O valor 3 apareceu na {num.index(3)+1}º posição')
+else:
+       print(f'O valor 3 não foi digitado')
+print(f'Os valores pares digitado foram ', end='')
+for n in num:
+       if n % 2 == 0:
+              print(n, end=' ')

d8f1496efdef7afb57413aa9c4f26001723307a7
andrewyamagata <67279172+andrewyamagata@users.noreply.github.com>
2021-03-21 20:14:41 -0300

    Lista de Preços com Tupla

--- Parte 03/Projeto 06.py
+++ Parte 03/Projeto 06.py
@@ -4,3 +4,11 @@ listagem = ('Lápis', 1.75,
             'Caderno', 15.90,
             'Estojo', 25,
             )
+print('-'*40)
+print(f'{"LISTAGEM DE PREÇOS":^40}')
+print('-'*40)
+for item in range (0, len(listagem)):
+    if pos % 2 == 0;
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print()
